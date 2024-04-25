68a6feedafb1ecac43dbff2cb3f9be0f69c98402
EmilieSommer <emilie.freltoft@gmail.com>
2024-04-25 17:53:22 +0200

    catManager

---
+++ Assets/Scripts/CatManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CatManager : MonoBehaviour
+{
+    public GameObject chosenCat;
+    public List<Color> chosenColors;
+
+    public void OnDoneButtonPressed()
+    {
+        // Instantiate chosenCat and apply chosenColors in the "stickerscroll" scene
+        SceneManager.LoadScene("stickerscroll");
+    }
+}
