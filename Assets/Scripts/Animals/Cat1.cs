71a0023c30c7774bd3dd1a2387e2353d653eba1e
Garioo <marius@qvarnstrom.dk>
2024-04-25 15:11:53 +0200

    Animations plus class inheritance

---
+++ Assets/Scripts/Animals/Cat1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat1 : Cat
+{
+    public void Start()
+    {
+        Speed();
+        LayDown();
+        GiveScripts();
+    }
+}
