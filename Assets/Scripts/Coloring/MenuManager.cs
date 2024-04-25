f1d7a889a7ff37084cd871c1fd9c27e191759bab
SoffeSof <Sofie.d.andersen@hotmail.dk>
2024-04-26 01:02:44 +0200

    instantiating cat

--- Assets/Scripts/Coloring/MenuManager.cs
+++ Assets/Scripts/Coloring/MenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -13,15 +14,21 @@ public class MenuManager : MonoBehaviour
     public GameObject[] puslingSprites = new GameObject[9];
     public GameObject[] mislingSprites = new GameObject[18];
 
+    public GameObject chosenCat;
+    public GameObject chosenCatPrefab;
+    public List<GameObject> chosenList = new List<GameObject>();
+    public List<Color> dataOfColors = new List<Color>();
+    public List<GameObject> catPrefabSprites = new List<GameObject>();
+
     public void Start()
     {
-        Musling.SetActive(false);
+        Musling.SetActive(true);
         Pusling.SetActive(false);
+        Misling.SetActive(false);
 
     }
     public void MuslingClick()
     {
-        Debug.Log(Musling.activeInHierarchy);
         switch (Musling.activeInHierarchy)
         {
             case true:
@@ -34,16 +41,12 @@ public class MenuManager : MonoBehaviour
                     Pusling.SetActive(false);
                     Musling.SetActive(true);
                 }
-                else if(Misling.activeInHierarchy)
+                else if (Misling.activeInHierarchy)
                 {
                     ResetColorsMisling();
                     Misling.SetActive(false);
                     Musling.SetActive(true);
                 }
-
-                
-
-
                 else if (!Pusling.activeInHierarchy || !Misling.activeInHierarchy)
                 {
                     Musling.SetActive(true);
@@ -184,9 +187,128 @@ public class MenuManager : MonoBehaviour
             }
         }
     }
-    // Update is called once per frame
-    void Update()
+
+    public void CheckChosenCat()
     {
-        
+        if (Pusling.activeInHierarchy == true)
+        {
+            chosenCat = Pusling;
+            // Check if the prefab is loaded successfully
+            if (chosenCatPrefab == null)
+            {
+                Debug.LogError("Failed to load the prefab");
+            }
+            chosenCat = Pusling;
+            foreach (GameObject obj in puslingSprites)
+            {
+                if (obj != null)
+                {
+                    chosenList.Add(obj);
+                }
+            }
+
+        }
+        else if (Musling.activeInHierarchy == true)
+        {
+            chosenCat = Musling;
+            foreach (GameObject obj in muslingSprites)
+            {
+                if (obj != null)
+                {
+                    chosenList.Add(obj);
+                }
+            }
+        }
+        else if (Misling.activeInHierarchy == true)
+        {
+            chosenCat = Misling;
+            foreach (GameObject obj in puslingSprites)
+            {
+                if (obj != null)
+                {
+                    chosenList.Add(obj);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("No cats are active");
+        }
+        chosenCatPrefab = chosenCat;
+    }
+
+    public void SaveData()
+    {
+        CheckChosenCat();
+        if (chosenCat.activeInHierarchy == true)
+        {
+            foreach (GameObject obj in chosenList)
+            {
+                if (obj != null)
+                {
+                    // Get the Renderer component of the GameObject
+                    SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+
+                    if (spriteRenderer != null)
+                    {
+                        dataOfColors.Add(spriteRenderer.color);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Renderer component not found on GameObject: " + obj.name);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning("Null GameObject found in the array!");
+                }
+            }
+        }
+    }
+
+    public void InstantiateCat()
+    {
+        GameObject instantiatedCat = Instantiate(chosenCatPrefab, Vector3.zero, Quaternion.identity);
+        GameObject[] allChildren = instantiatedCat.GetComponentsInChildren<GameObject>(true);
+
+        // Add all game objects to the list
+        foreach (GameObject child in allChildren)
+        {
+            catPrefabSprites.Add(child);
+        }
+        foreach (GameObject obj in catPrefabSprites)
+        {
+            if (obj != null)
+            {
+                // Get the Renderer component of the GameObject
+                SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+
+                if (spriteRenderer != null)
+                {
+                    int index = 0;
+                    if(index > catPrefabSprites.Count)
+                    {
+                        spriteRenderer.color = dataOfColors[index];
+                        index++;
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning("Renderer component not found on GameObject: " + obj.name);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Null GameObject found in the array!");
+            }
+        }
+
+    }
+
+    public string sceneName;
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
+
