a02ad4854337bf85ae232d19862b65fa8ad68252
Garioo <marius@qvarnstrom.dk>
2024-04-26 08:43:40 +0200

    Push det

---
+++ Assets/Scripts/CustomCatFinal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCatFinal : MonoBehaviour
+{
+    public GameObject npcPus;
+    public GameObject npcMus;
+    public GameObject npcMis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
